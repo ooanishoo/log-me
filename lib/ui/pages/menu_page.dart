19cb0e56f5c419412b550020b72f542022e7ffb7
ooanishoo <anish.maharjan12341@gmail.com>
2020-02-18 09:44:43 +1100

    :new Initialize seperate components for reusability

--- lib/ui/pages/menu_page.dart
+++ lib/ui/pages/menu_page.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class MenuPage extends StatefulWidget {
   MenuPage({Key key}) : super(key: key);
@@ -8,16 +10,43 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
+  List<bool> _selections = [false, true];
+  bool mode;
+
+  void themeMode() async {
+    bool val = await DynamicTheme.of(context).loadBrightness();
+    setState(() => this.mode = val);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    themeMode();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Menu Page'),
+        title: Text('Menu'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Menu Page'),
-        ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.brightness_3),
+            title: Text('Dark mode'),
+            trailing: LiteRollingSwitch(
+              value: mode,
+              colorOff: Colors.grey,
+              animationDuration: Duration(milliseconds: 50),
+              onChanged: (bool state) {
+                print('turned ${(state) ? 'on' : 'off'}');
+                DynamicTheme.of(context)
+                    .setBrightness(state ? Brightness.dark : Brightness.light);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
