f390b9d9868e680125ed1f01ba4908a1c05bf802
ooanishoo <anish.maharjan12341@gmail.com>
2020-02-24 21:08:52 +1100

    Show selected workout below the calendar view with right date format

--- lib/ui/pages/list_workout_page.dart
+++ lib/ui/pages/list_workout_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:scoped_log_me/models/exercise.dart';
 import 'package:scoped_log_me/scoped_models/app_model.dart';
+import 'package:scoped_log_me/ui/pages/edit_workout_page.dart';
 import 'package:scoped_log_me/ui/views/list_workout_view.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:scoped_log_me/scoped_models/workout_model.dart';
@@ -22,8 +25,8 @@ class ListWorkoutPageState extends State<ListWorkoutPage> {
   String filter = '';
   List<Exercise> selectedExercises = [];
   CalendarController _calendarController;
-  List<dynamic>_selectedEvents =[];
-
+  List<dynamic> _selectedEvents = [];
+  int page = 0;
 
   @override
   void initState() {
@@ -35,7 +38,7 @@ class ListWorkoutPageState extends State<ListWorkoutPage> {
       setState(() => filter = _controller.text);
     });
     _calendarController = CalendarController();
-    _selectedEvents=[];
+    _selectedEvents = [];
   }
 
   @override
@@ -48,74 +51,98 @@ class ListWorkoutPageState extends State<ListWorkoutPage> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
+      initialIndex: this.page,
       child: Scaffold(
         appBar: AppBar(
           title: Text('History'),
           centerTitle: false,
           bottom: TabBar(
             tabs: [
-              Tab(text: "Calendar",),
-              Tab(text: "Workouts",),
+              Tab(
+                text: "Calendar",
+              ),
+              Tab(
+                text: "Workouts",
+              ),
             ],
+            onTap: ((int index) => setState(() {
+                  this.page = index;
+                })),
           ),
         ),
-        // floatingActionButton: FloatingActionButton(
-        //     onPressed: (() => widget.workoutModel.getEvents())),
-        body: TabBarView(
-          children: [
-            TableCalendar(
+        body: TabBarView(children: [
+          Column(
+            children: <Widget>[
+              TableCalendar(
                 events: widget.workoutModel.getEvents(),
                 calendarController: _calendarController,
                 startingDayOfWeek: StartingDayOfWeek.monday,
                 initialCalendarFormat: CalendarFormat.month,
                 calendarStyle: CalendarStyle(
                   markersColor: Theme.of(context).accentColor,
-                  selectedColor: Theme.of(context).accentColor.withOpacity(0.25),
+                  selectedColor:
+                      Theme.of(context).accentColor.withOpacity(0.25),
                   holidayStyle: TextStyle(color: Colors.white),
                   weekendStyle: TextStyle(color: Theme.of(context).buttonColor),
                 ),
                 headerStyle: HeaderStyle(
-                  centerHeaderTitle: false,
-                  decoration: BoxDecoration(
-                    //color: Colors.green,
-                  ),
-                  formatButtonVisible: true,
-                  formatButtonDecoration: BoxDecoration(
-                    //color: Theme.of(context).buttonColor
-                    color: Theme.of(context).accentColor.withOpacity(0.25),
-                    borderRadius: new BorderRadius.all(
-                                              new Radius.circular(8.0))
-                  ),
-                  leftChevronIcon: Icon(
-                    Icons.chevron_left,
-                    color: Theme.of(context).buttonColor
-                  ),
-                  rightChevronIcon: Icon(
-                    Icons.chevron_right,
-                    color: Theme.of(context).buttonColor
-                  )
-                ),
+                    centerHeaderTitle: false,
+                    decoration: BoxDecoration(
+                        //color: Colors.green,
+                        ),
+                    formatButtonVisible: true,
+                    formatButtonDecoration: BoxDecoration(
+                        //color: Theme.of(context).buttonColor
+                        color: Theme.of(context).accentColor.withOpacity(0.25),
+                        borderRadius:
+                            new BorderRadius.all(new Radius.circular(8.0))),
+                    leftChevronIcon: Icon(Icons.chevron_left,
+                        color: Theme.of(context).buttonColor),
+                    rightChevronIcon: Icon(Icons.chevron_right,
+                        color: Theme.of(context).buttonColor)),
                 onDaySelected: (date, events) {
                   setState(() {
                     this._selectedEvents = events;
                   });
                 },
               ),
-            ScopedModel<WorkoutModel>(
-              model: widget.workoutModel,
-              child: ListWorkout(filter: filter),
-            ),
-          ]),   
-      ), 
+              ..._selectedEvents.map((workout) => ListTile(
+                    title: Text(workout.name),
+                    subtitle: new Text(
+                      new DateFormat.MMMd().format(workout.date).toString(),
+                      style: TextStyle(color: Theme.of(context).iconTheme.color),
+                    ),
+                    trailing: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(
+                            style: BorderStyle.solid,
+                            color: Theme.of(context).iconTheme.color,
+                          )),
+                      child: Text(
+                        'View Workout',
+                        style: TextStyle(color: Theme.of(context).buttonColor),
+                      ),
+                      onPressed: () {
+                        HapticFeedback.heavyImpact();
+                        WorkoutModel newWorkoutModel = WorkoutModel();
+                        newWorkoutModel.currentWorkout = workout;
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => EditWorkoutPage(
+                                  workoutModel: newWorkoutModel,
+                                  model: new AppModel(),
+                                )));
+                      },
+                    ),
+                  )),
+            ],
+          ),
+          ScopedModel<WorkoutModel>(
+            model: widget.workoutModel,
+            child: ListWorkout(filter: filter),
+          ),
+        ]),
+      ),
     );
-          //Column(
-          // children: <Widget>[
-          //   // Exercise List
-          //   ScopedModel<WorkoutModel>(
-          //     model: widget.workoutModel,
-          //     child: ListWorkout(filter: filter),
-          //   ),
-          // ],
-        //));
   }
 }
