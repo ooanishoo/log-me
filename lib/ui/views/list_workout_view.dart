719be3d36cace3e7692d58b52642262593e2fbd3
ooanishoo <anish.maharjan12341@gmail.com>
2020-02-24 20:01:56 +1100

    Add Edit and Delete icons in the listview of workouts

--- lib/ui/views/list_workout_view.dart
+++ lib/ui/views/list_workout_view.dart
@@ -3,7 +3,6 @@ import 'package:scoped_log_me/models/workout.dart';
 import 'package:scoped_log_me/scoped_models/app_model.dart';
 
 import 'package:scoped_log_me/scoped_models/workout_model.dart';
-import 'package:scoped_log_me/service_locator.dart';
 import 'package:scoped_log_me/ui/pages/edit_workout_page.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -21,48 +20,56 @@ class _ListWorkoutState extends State<ListWorkout> {
   Widget build(BuildContext context) {
     return ScopedModelDescendant<WorkoutModel>(
       builder: (x, y, model) {
-        return Expanded(
-            // GestureDetector is added for hiding keyboard when swiping down
-            child: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onPanDown: (_) {
-            FocusScope.of(context).requestFocus(FocusNode());
-          },
-          child: ListView.separated(
-            separatorBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-              child: Divider(height: 0),
-            ),
-            itemCount: model.workouts.length,
-            itemBuilder: (context, index) {
-              // store the object to a local variable
-              Workout workout = model.workouts[index];
-              // Display all when search filter is empty
-              return widget.filter == null || widget.filter == ''
-                  ? workoutTile(model, workout)
-                  : workout.name.contains(widget.filter)
-                      ? workoutTile(model, workout)
-                      : new Container();
-            },
+        return ListView.separated(
+          separatorBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            child: Divider(height: 0),
           ),
-        ));
+          itemCount: model.workouts.length,
+          itemBuilder: (context, index) {
+            // store the object to a local variable
+            Workout workout = model.workouts[index];
+            // Display all when search filter is empty
+            return widget.filter == null || widget.filter == ''
+                ? workoutTile(model, workout)
+                : workout.name.contains(widget.filter)
+                    ? workoutTile(model, workout)
+                    : new Container();
+          },
+          //   ),
+          // )
+        );
       },
     );
   }
 
   Widget workoutTile(WorkoutModel model, Workout workout) {
     return ListTile(
-      title: new Text(workout.name),
-      subtitle: new Text(workout.date.toString()),
-      onTap: () {
-        WorkoutModel newWorkoutModel = WorkoutModel();
-        newWorkoutModel.currentWorkout = workout;
-        Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => EditWorkoutPage(
-              workoutModel:newWorkoutModel,
-              model: new AppModel(),
-            )));
-      },
-    );
+        title: new Text(workout.name),
+        subtitle: new Text(workout.date.toString()),
+        trailing: ButtonBar(
+          alignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                WorkoutModel newWorkoutModel = WorkoutModel();
+                newWorkoutModel.currentWorkout = workout;
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => EditWorkoutPage(
+                          workoutModel: newWorkoutModel,
+                          model: new AppModel(),
+                        )));
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: (() {
+                model.removeWorkout(workout);
+              }),
+            ),
+          ],
+        ));
   }
 }
